75124657432319482a538f20f515d31aa662a4e7
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 19:23:48 +0300

    magazines class done-extends book

--- src/com/workintech/Library/models/Magazines.java
+++ src/com/workintech/Library/models/Magazines.java
@@ -1,4 +1,14 @@
 package com.workintech.Library.models;
 
-public class Magazines {
+import java.util.Date;
+
+public class Magazines extends Book{
+    public Magazines(Long bookId, Author author, Person owner, String name, Double price, String status, String edition, Date dateOfPurchase) {
+        super(bookId, author, owner, name, price, status, edition, dateOfPurchase);
+    }
+
+    @Override
+    public String toString() {
+        return "Magazines{}";
+    }
 }
