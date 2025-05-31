393a42fd7ec7beb8f359b490a5cf2bafc58a59c6
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 18:55:41 +0300

    person class done

--- src/com/workintech/Library/models/Person.java
+++ src/com/workintech/Library/models/Person.java
@@ -1,4 +1,15 @@
 package com.workintech.Library.models;
 
-public class Person {
+public abstract class Person {
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void whoyouare();
 }
