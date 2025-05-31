37778835f0d60b990f384e69f1708720e84ed246
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 19:27:04 +0300

    student class done - extends member record

--- src/com/workintech/Library/models/Student.java
+++ src/com/workintech/Library/models/Student.java
@@ -1,4 +1,22 @@
 package com.workintech.Library.models;
 
-public class Student {
+import java.util.Date;
+
+public class Student extends MemberRecord{
+    private String studentId;
+
+    public Student(Long memberId, String type, Date dateOfMembership, int noBooksIssued, String name, String phoneNo, String address, String studentId) {
+        super(memberId, type, dateOfMembership, noBooksIssued, name, phoneNo, address);
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+
+    @Override
+    public void whoyouare() {
+        System.out.println("Student Name: " + getName() + ", Student ID: " + studentId);
+    }
 }
