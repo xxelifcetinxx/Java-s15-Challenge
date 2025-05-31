fb3ab701310424f4084b812aa87b885312929ecd
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 19:21:09 +0300

    study books class done -extends book

--- src/com/workintech/Library/models/StudyBooks.java
+++ src/com/workintech/Library/models/StudyBooks.java
@@ -1,4 +1,15 @@
 package com.workintech.Library.models;
 
-public class StudyBooks {
+import java.util.Date;
+
+public class StudyBooks extends Book{
+
+    public StudyBooks(Long bookId, Author author, Person owner, String name, Double price, String status, String edition, Date dateOfPurchase) {
+        super(bookId, author, owner, name, price, status, edition, dateOfPurchase);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
