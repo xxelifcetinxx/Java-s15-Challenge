cbac45de47a8525e67d3598b0dc578f821bbad61
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 19:19:43 +0300

    journals class done -extends book

--- src/com/workintech/Library/models/Journals.java
+++ src/com/workintech/Library/models/Journals.java
@@ -1,4 +1,14 @@
 package com.workintech.Library.models;
 
-public class Journals {
+import java.util.Date;
+
+public class Journals extends Book{
+    public Journals(Long bookId, Author author, Person owner, String name, Double price, String status, String edition, Date dateOfPurchase) {
+        super(bookId, author, owner, name, price, status, edition, dateOfPurchase);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
