83a752be6c36f842543bb89306fc2559a72ea68f
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 18:57:23 +0300

    person class done -extends person

--- src/com/workintech/Library/models/Author.java
+++ src/com/workintech/Library/models/Author.java
@@ -1,4 +1,31 @@
 package com.workintech.Library.models;
 
-public class Author {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Author extends Person {
+    private List<Book> books;
+
+    public Author(String name) {
+        super(name);
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public void whoyouare() {
+        System.out.println("Author Name: " + getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Author Name: " + getName() + ", Books: " + books.size();
+    }
 }
