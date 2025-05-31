0813cc766a9b99915735c56999b85dfa85ad46c8
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 19:01:26 +0300

    author&reader class done -extends person

--- src/com/workintech/Library/models/Reader.java
+++ src/com/workintech/Library/models/Reader.java
@@ -1,4 +1,18 @@
 package com.workintech.Library.models;
 
-public class Reader {
+public class Reader extends Person {
+
+    public Reader(String name) {
+        super(name);
+    }
+
+    @Override
+    public void whoyouare() {
+        System.out.println("Reader Name: " + getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Reader Name: " + getName();
+    }
 }
